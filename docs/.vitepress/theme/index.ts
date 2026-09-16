6c0445acd363daeac719c7d2a47a496a8bc277dc
carlor <106606@qq.com>
2026-09-16 22:26:45 +0800

    style: align sidebar and top navigation with NapCat

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
 import HomePage from './HomePage.vue'
 import ReleasePanel from './ReleasePanel.vue'
+import '../../../node_modules/harmonyos-sans-sc-webfont-splitted/dist/Regular.css'
+import '../../../node_modules/harmonyos-sans-sc-webfont-splitted/dist/Medium.css'
+import '../../../node_modules/harmonyos-sans-sc-webfont-splitted/dist/Bold.css'
 import './style.css'
+import './navigation.css'
 
 export default {
   extends: DefaultTheme,
